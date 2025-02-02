7b7c840f714a373780f6b466998cf13bff097f9e
driab77 <144946823+driab77@users.noreply.github.com>
2025-02-01 22:26:44 -0700

    Fixed the bordering issue.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,3 +7,36 @@ function toggleMenu() {
         menuContent.style.flexDirection = 'column';
     }
 }
+
+function viewHandler(event) {
+    const clickedImg = event.target;
+    const imgSrc = clickedImg.getAttribute("src");
+    const imgNameParts = imgSrc.split("-");
+    const highResImage = imgNameParts[0] + "-full.jpeg";  
+
+    const viewer = document.getElementById("imageviewer");
+
+    if (viewer) {
+        viewer.style.display = "flex";  
+        document.getElementById("viewerImg").src = highResImage; 
+    }
+}
+
+function closeViewer() {
+    const viewer = document.getElementById("imageviewer");
+    if (viewer) {
+        viewer.style.display = "none";  
+    }
+}
+
+document.querySelector(".closeviewer").addEventListener("click", closeViewer);
+
+document.querySelectorAll(".gallery img").forEach(img => {
+    img.addEventListener("click", viewHandler);
+});
+
+document.getElementById("imageviewer").addEventListener("click", function(event) {
+    if (event.target === this) {
+        closeViewer();
+    }
+});
